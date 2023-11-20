c020258d51fc836c11a86c2573daba66005fd33c
aanatema <romane.boireau@gmail.com>
2023-11-20 12:21:56 +0100

    ajout de la wipcard

--- src/Components/ProjectsNavbar.js
+++ src/Components/ProjectsNavbar.js
@@ -5,11 +5,15 @@ function ProjectsNavbar() {
 
  return (
     // problems : the tab doesn't stay activ when we click on it because it redirect to the page of the project in question 
-    // how do i fix the active tab
-    <Nav variant="tabs" data-bs-theme="dark" defaultActiveKey=""
-    className="mt-5 ms-5 me-5">
+    // how do i fix the active tab ? 
+    <Nav variant="tabs" data-bs-theme="dark" defaultActiveKey="/projects"
+    className="mt-5 mx-5">
+        <Nav.Item>
+        <Nav.Link href="/projects" eventKey="0">Tous les projets</Nav.Link>
+      </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="/projects/consonea" eventKey="1">Consonéa</Nav.Link>
+        {/* the tab stays active but doesn't redirect towards the page project (no href)*/}
+        <Nav.Link eventKey="/projects/consonea">Consonéa</Nav.Link>
       </Nav.Item>
       <Nav.Item>
         <Nav.Link href="/projects/choose_your_career" eventKey="2">Choose your career</Nav.Link>
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,9 +1,14 @@
 import NavBar from "../Components/Navbar"
+import WipCard from "../Components/WipCard"
 
 function HomePage (){
-    return (
-<NavBar />   
 
+
+    return (
+        <>
+        <NavBar /> 
+        <WipCard />
+        </>
 
  )
 };
