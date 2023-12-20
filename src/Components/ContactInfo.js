720febe10c9bb6a12fee93e5bc46553e7658ddc4
aanatema <romane.boireau@gmail.com>
2023-12-20 18:56:25 +0100

    la page se reload après l'envoie du formulaire de contact

--- src/Components/ContactInfo.js
+++ src/Components/ContactInfo.js
@@ -4,26 +4,38 @@ import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import FloatingLabel from "react-bootstrap/FloatingLabel";
 import Form from "react-bootstrap/Form";
-import { useState } from "react";
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import "../index.css";
 
 function ContactInfo() {
-  const [validated, setValidated] = useState(false);
+ 
+  const form = useRef();
 
-  const handleSubmit = (event) => {
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-    setValidated(true);
+    emailjs.sendForm(
+        "service_39lsro9",
+        "template_8i7nqj7",
+        form.current,
+        "Q-vLiAGXssjwGb0gX"
+      ).then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
   };
 
   return (
     <Container>
-      <h1 md={6} lg={6}className="mt-5 mb-3">Envie d'échanger ? Laissez moi message !</h1>
-      <Form noValidate validated={validated} onSubmit={handleSubmit}>
+      <h1 md={6} lg={6} className="mt-5 mb-3">
+        Envie d'échanger ? Laissez moi message !
+      </h1>
+      <Form ref={form} onSubmit={sendEmail}>
         <Row className="mt-5 g-2">
           <Col md>
             <FloatingLabel
@@ -35,12 +47,13 @@ function ContactInfo() {
                 required
                 type="text"
                 className="contact_infos"
+                name="user_nom"
                 as="textarea"
                 placeholder="Leave a comment here"
               />
-               <Form.Control.Feedback type="invalid">
-               Veuillez vérifier votre nom.
-          </Form.Control.Feedback>
+              <Form.Control.Feedback type="invalid">
+                Veuillez vérifier votre nom.
+              </Form.Control.Feedback>
             </FloatingLabel>
           </Col>
           <Col md>
@@ -53,12 +66,13 @@ function ContactInfo() {
                 required
                 type="text"
                 className="contact_infos"
+                name="user_prenom"
                 as="textarea"
                 placeholder="Leave a comment here"
               />
-               <Form.Control.Feedback type="invalid">
-               Veuillez vérifier votre prénom.
-          </Form.Control.Feedback>
+              <Form.Control.Feedback type="invalid">
+                Veuillez vérifier votre prénom.
+              </Form.Control.Feedback>
             </FloatingLabel>
           </Col>
           <FloatingLabel
@@ -69,28 +83,30 @@ function ContactInfo() {
             <Form.Control
               required
               className="contact_infos"
+              name="user_email"
               type="email"
               placeholder="name@example.com"
             />
-             <Form.Control.Feedback type="invalid">
-            Veuillez vérifier votre adresse mail.
-          </Form.Control.Feedback>
+            <Form.Control.Feedback type="invalid">
+              Veuillez vérifier votre adresse mail.
+            </Form.Control.Feedback>
           </FloatingLabel>
           <FloatingLabel
-              controlId="floatingTextarea"
-              label="Numéro de téléphone"
-              className="contact_infos mb-3"
-            >
-              <Form.Control
-                type="text"
-                className="contact_infos"
-                as="textarea"
-                placeholder="0612345678"
-              />
-              <Form.Control.Feedback type="invalid">
-            Veuillez vérifier votre numéro de téléphone
-          </Form.Control.Feedback>
-            </FloatingLabel>
+            controlId="floatingTextarea"
+            label="Numéro de téléphone"
+            className="contact_infos mb-3"
+          >
+            <Form.Control
+              type="text"
+              className="contact_infos"
+              name="user_tel"
+              as="textarea"
+              placeholder="0612345678"
+            />
+            <Form.Control.Feedback type="invalid">
+              Veuillez vérifier votre numéro de téléphone
+            </Form.Control.Feedback>
+          </FloatingLabel>
           <FloatingLabel
             controlId="floatingTextarea"
             label="Votre message"
@@ -100,20 +116,23 @@ function ContactInfo() {
               required
               type="tel"
               className="contact_infos"
+              name="user_message"
               style={{ height: "200px" }}
               as="textarea"
               placeholder="Leave a comment here"
             />
-             <Form.Control.Feedback type="invalid">
-            N'oubliez pas d'entrer votre message.
-          </Form.Control.Feedback>
+            <Form.Control.Feedback type="invalid">
+              N'oubliez pas d'entrer votre message.
+            </Form.Control.Feedback>
           </FloatingLabel>
         </Row>
-        <Button className="submit_btn mt-4" type="submit">
+        <Button className="submit_btn mt-4" onClick={() => window.location.reload(false)} type="submit" value="Send">
           Envoyer
         </Button>
       </Form>
     </Container>
+  
   );
+  
 }
 export default ContactInfo;
--- src/Pages/ProjectChooseYourCareer.js
+++ src/Pages/ProjectChooseYourCareer.js
@@ -13,7 +13,7 @@ function ProjectChooseYourCareer() {
     <>
       <NavBar />
       <Container className="mt-5">
-      <div>
+        <div>
           <h1 className="project_title">CHOOSE YOUR CAREER</h1>
           <h2 className="project_subtitle">Projet de data visualisation</h2>
         </div>
@@ -26,75 +26,86 @@ function ProjectChooseYourCareer() {
           LE PROJET SUR GITHUB
         </Button>
         <div className="mt-5">
-        <Row>
-          <Col className="project_image " xs={12} sm={12} md={12} lg={5}>
-            <Image src={cyc_result_44} fluid />
-          </Col>
-          <Col className="" xs={12} sm={12} md={12} lg={7}>
-            <Accordion className="accordion mb-5">
-              <Accordion.Item eventKey="0">
-                <Accordion.Header>
-                  <strong>CONTEXTE ET OBJECTIFS</strong>
-                </Accordion.Header>
-                <Accordion.Body>
-                  Comprendre le fonctionnement d'une API en faisant des appels
-                  API.<br></br> Afficher les données récupérées avec{" "}
-                  <strong>Chart.js</strong>.
-                </Accordion.Body>
-              </Accordion.Item>
-              <Accordion.Item eventKey="1">
-                <Accordion.Header>
-                  <strong>ORGANISATION</strong>
-                </Accordion.Header>
-                <Accordion.Body>
-                  Code en solo, pair et mob programming avec code review
-                  régulier.<br></br>
-                  Utilisation de trello pour se répartir les tâches et
-                  déterminer les priorités.<br></br>
-                  Mise au point régulière avec le groupe afin de savoir comment
-                  le projet se passe individuellement et si il y a des besoins
-                  particuliers qui ressortent.<br></br>
-                </Accordion.Body>
-              </Accordion.Item>
-              <Accordion.Item eventKey="2">
-                <Accordion.Header>
-                  <strong>TECHNOS</strong>
-                </Accordion.Header>
-                <Accordion.Body>
-                  <strong>Langages :</strong> HTML, CSS, JavaScript <br></br>
-                  <strong>Technos :</strong> Chart.js, API Pôle Emploi, Postman
-                </Accordion.Body>
-              </Accordion.Item>
-              <Accordion.Item eventKey="3">
-                <Accordion.Header>
-                  <strong>RESULTATS ET AXES D'AMELIORATION</strong>
-                </Accordion.Header>
-                <Accordion.Body>
-                  La barre de recherche est fonctionnelle, les résultats
-                  affichés sont bien ceux des département recherchés. On peut
-                  séléctionner ou déselctionner les données sur les graphiques.
-                  <br></br>
-                  Mettre une image en temps que placeholder lorsqu'il n'y a pas
-                  de graphiques, ajouter un loading.
-                </Accordion.Body>
-              </Accordion.Item>
-            </Accordion>
-            <Button
-              className="mb-5 me-5"
-              href="/projects"
-              variant="outline-light"
-            >
-              Revenir aux projets
-            </Button>
-            <Button
-              className="mb-5 "
-              href="/projects/kind_glyphs"
-              variant="outline-light"
-            >
-              Projet suivant
-            </Button>
-          </Col>
-        </Row>
+          <Row>
+            <Col className="project_image " xs={12} sm={12} md={12} lg={5}>
+              <Image src={cyc_result_44} fluid />
+            </Col>
+            <Col className="" xs={12} sm={12} md={12} lg={7}>
+              <Accordion className="accordion mb-5">
+                <Accordion.Item eventKey="0">
+                  <Accordion.Header>
+                    <strong>CONTEXTE ET OBJECTIFS</strong>
+                  </Accordion.Header>
+                  <Accordion.Body>
+                    Comprendre le fonctionnement d'une API en faisant des appels
+                    API.<br></br> Afficher les données récupérées avec{" "}
+                    <strong>Chart.js</strong>.
+                  </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1">
+                  <Accordion.Header>
+                    <strong>ORGANISATION</strong>
+                  </Accordion.Header>
+                  <Accordion.Body>
+                    Code en solo, pair et mob programming avec code review
+                    régulier.<br></br>
+                    Utilisation de trello pour se répartir les tâches et
+                    déterminer les priorités.<br></br>
+                    Mise au point régulière avec le groupe afin de savoir
+                    comment le projet se passe individuellement et si il y a des
+                    besoins particuliers qui ressortent.<br></br>
+                  </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="2">
+                  <Accordion.Header>
+                    <strong>TECHNOS</strong>
+                  </Accordion.Header>
+                  <Accordion.Body>
+                    <strong>Langages :</strong> HTML, CSS, JavaScript <br></br>
+                    <strong>Technos :</strong> Chart.js, API Pôle Emploi,
+                    Postman
+                  </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="3">
+                  <Accordion.Header>
+                    <strong>RESULTATS ET AXES D'AMELIORATION</strong>
+                  </Accordion.Header>
+                  <Accordion.Body>
+                    La barre de recherche est fonctionnelle, les résultats
+                    affichés sont bien ceux des département recherchés. On peut
+                    séléctionner ou déselctionner les données sur les
+                    graphiques.
+                    <br></br>
+                    Mettre une image en temps que placeholder lorsqu'il n'y a
+                    pas de graphiques, ajouter un loading.
+                  </Accordion.Body>
+                </Accordion.Item>
+              </Accordion>
+              <Button
+                className="mb-5 me-5"
+                href="/projects"
+                variant="outline-light"
+              >
+                Revenir aux projets
+              </Button>
+            </Col>
+            <Col>
+              <Button
+                className="mb-5 me-4 "
+                href="/projects/kind_glyphs"
+                variant="outline-light"
+              >
+                Projet précédant
+              </Button>
+              <Button
+                className="mb-5 "
+                href="/projects/kind_glyphs"
+                variant="outline-light"
+              >
+                Projet suivant
+              </Button>
+            </Col>
+          </Row>
         </div>
       </Container>
     </>
