943daf030abb9618a901a6ac7905c7f06de75672
aanatema <romane.boireau@gmail.com>
2024-01-25 11:42:26 +0100

    changement texte bannière

--- src/Components/ChangesAlert.js
+++ src/Components/ChangesAlert.js
@@ -4,7 +4,7 @@ function ChangesAlert() {
   return (
     <>
         <Alert variant="info" className="mt-3 mx-2" style={{ textAlign: "center" }}>
-          <p className='my-0'> Ceci est la V.1 de mon portfolio, il est amené à évoluer. Pour voir ce qui a changé depuis la dernière fois, c'est <Alert.Link href="/updates" >ici</Alert.Link> !</p>
+          <p className='my-0'> Bienvenue ! Ceci est la première version fonctionnelle de mon portfolio, il est donc amené à évoluer. Pour voir ce qui a changé depuis la dernière fois, c'est <Alert.Link href="/updates" >ici</Alert.Link> !</p>
         </Alert>
     </>
   );
